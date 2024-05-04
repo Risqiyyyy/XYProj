81148039fcd19ae8fb14d4b5b78d92054c3c04d2
ezazee <tegar.firman215@gmail.com>
2024-05-04 13:02:43 +0700

    first

---
+++ resources/views/admin/partials/master-css.blade.php
@@ -0,0 +1,4 @@
+<link rel="stylesheet" href="{{ URL::asset('admin/assets/css/app.min.css') }}">
+<link rel="stylesheet" href="{{ URL::asset('admin/assets/css/icons.min.css') }}">
+<link rel="stylesheet" href="{{ URL::asset('admin/assets/css/bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ URL::asset('admin/assets/js/app.js') }}">
